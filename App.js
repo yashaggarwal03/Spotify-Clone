a6d26f5c45714fb147c4dcd0d4371cdd21929fcb
Yash <yash.agg2000@gmail.com>
2021-09-23 19:49:52 +0530

    Made some changes

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import {NavigationContainer, DarkTheme} from '@react-navigation/native';
 import {StatusBar} from 'react-native';
 
 import BottomTab from './src/components/BottomTab';
-import FirstStack from './src/navigations/FirstStack';
 
 const App = () => {
   return (
