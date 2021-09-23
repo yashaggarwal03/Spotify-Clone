501beaf5188ba70b5122dc82ff603aca04075e5c
Yash <yash.agg2000@gmail.com>
2021-09-24 00:00:00 +0530

    Songs Data

---
+++ src/components/SongList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const SongList = () => {
+    return (
+        <View>
+            <Text style={{color: "#fff"}}>Songs List</Text>
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({})
+
+export default SongList;
