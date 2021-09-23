f6e75092345d147e98c55662f22bcf7c42006712
Yash <yash.agg2000@gmail.com>
2021-09-24 00:02:45 +0530

    Songs Data

---
+++ src/models/AlbumData.js
@@ -0,0 +1,32 @@
+const Songs =[ 
+  {
+    id: '1',
+    image: require('../assets/Shershaah.jpg'),
+    name: 'Shershaah',
+    year: 'Album • 2020',
+    songs: [
+      {
+        id: '1',
+        title: 'Raataan Lambiyan',
+        artist: 'Jubin Nautiyal, Asees Kaur',
+      },
+      {
+        id: '2',
+        title: 'Ranjha',
+        artist: 'B Praak, Jasleen Royal',
+      },
+      {
+        id: '3',
+        title: 'Kabhii Tumhe',
+        artist: 'Darshan Raval, Javed-Mohsin',
+      },
+      {
+        id: '4',
+        title: 'Mann Bharryaa 2.0',
+        artist: 'B Praak',
+      },
+    ],
+  }
+];
+
+export default Songs;
