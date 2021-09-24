a3a2af2f7c1723c4e288c235552677964e33d33d
Yash <yash.agg2000@gmail.com>
2021-09-25 00:13:51 +0530

    Album Screen final

--- src/screens/AlbumScreen.js
+++ src/screens/AlbumScreen.js
@@ -1,38 +1,31 @@
 import React from 'react';
-import {Image, StyleSheet, Text, View} from 'react-native';
-import {
-  widthPercentageToDP as wp,
-  heightPercentageToDP as hp,
-} from 'react-native-responsive-screen';
+import {FlatList, Image, StyleSheet, Text, View} from 'react-native';
 
+//Importing Components and Data
+import AlbumHeader from '../components/AlbumHeader';
 import SongList from '../components/SongList';
-
 import SongsData from '../models/AlbumData';
 
 const AlbumScreen = ({route}) => {
   console.log(route.params);
   return (
     <View>
-      <View style={styles.container}>
-        <Image style={styles.imageStyle} source={SongsData[0].image} />
-      </View>
-      <Text style={{color: '#fff'}}>Hello Album Screen</Text>
-      <SongList />
+      
+      <FlatList
+        data={SongsData.songs}
+        renderItem={({item}) => (
+          <SongList icon={item.icon} title={item.title} artist={item.artist} />
+        )}
+        keyExtractor={item => item.id}
+        ListHeaderComponent={<AlbumHeader header={SongsData}/>}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {   
-    alignItems: 'center',
-    height:hp(40),
-    width: wp(50),
-    backgroundColor:'red'
-  },
-  imageStyle: {
-    resizeMode: 'contain',
-    height: hp(40),
-    width: wp(50)
+  container: {
+    flex: 1,
   },
 });
 
