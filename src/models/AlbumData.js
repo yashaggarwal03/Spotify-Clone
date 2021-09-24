a76dca258a112e3acd38376482d973136828caf3
Yash <yash.agg2000@gmail.com>
2021-09-24 13:57:49 +0530

    Songs Data

--- src/models/AlbumData.js
+++ src/models/AlbumData.js
@@ -3,28 +3,32 @@ const Songs =[
     id: '1',
     image: require('../assets/Shershaah.jpg'),
     name: 'Shershaah',
-    year: 'Album • 2020',
+    year: 'Album • 2021',
     songs: [
       {
         id: '1',
+        icon: require('../assets/Shershaah.jpg'),
         title: 'Raataan Lambiyan',
         artist: 'Jubin Nautiyal, Asees Kaur',
       },
       {
         id: '2',
+        icon: require('../assets/Shershaah.jpg'),
         title: 'Ranjha',
         artist: 'B Praak, Jasleen Royal',
       },
       {
         id: '3',
+        icon: require('../assets/Shershaah.jpg'),
         title: 'Kabhii Tumhe',
         artist: 'Darshan Raval, Javed-Mohsin',
       },
       {
         id: '4',
+        icon: require('../assets/Shershaah.jpg'),
         title: 'Mann Bharryaa 2.0',
         artist: 'B Praak',
-      },
+      },  
     ],
   }
 ];
