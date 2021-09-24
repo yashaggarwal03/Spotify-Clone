5571fde65e6908d78254231411b9be44c201f000
Yash <yash.agg2000@gmail.com>
2021-09-24 13:45:35 +0530

    Songs list component

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,15 +1,53 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import {Image, StyleSheet, Text, View} from 'react-native';
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from 'react-native-responsive-screen';
 
-const SongList = () => {
-    return (
-        <View>
-            <Text style={{color: "#fff"}}>Songs List</Text>
-        </View>
-    )
-}
+import Entypo from 'react-native-vector-icons/Entypo';
 
+const SongList = props => {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.icon} source={props.icon} />
+      <View style={styles.textContainer}>
+        <Text allowFontScaling={false} style={styles.title}>{props.title}</Text>
+        <Text allowFontScaling={false} style={styles.artist}>{props.artist}</Text>
+      </View>
+      <View style={styles.options}>
+        <Entypo name="dots-three-vertical" color="#a6a6a6" size={23} />
+      </View>
+    </View>
+  );
+};
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    margin: hp(1.5),
+  },
+  textContainer: {
+    padding: wp(1.5),
+  },
+  options: {
+    position: 'absolute',
+    right: wp(1),
+    alignSelf: 'center',
+  },
+  icon: {
+    resizeMode: 'contain',
+    height: hp(8),
+    width: wp(18),
+  },
+  title: {
+    color: '#fff',
+    fontSize: hp(2.4),
+  },
+  artist: {
+    color: '#a6a6a6',
+    fontSize: hp(1.8),
+  },
+});
 
 export default SongList;
