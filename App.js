dc46237f3b7baacaf4600a712fe1dec3b96eda98
Yash <yash.agg2000@gmail.com>
2021-09-24 13:49:05 +0530

    Main App File

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {NavigationContainer, DarkTheme} from '@react-navigation/native'; 
 import {StatusBar} from 'react-native';
 
-import BottomTab from './src/components/BottomTab';
+import BottomTab from './src/navigation/BottomTab';
 
 const App = () => {
   return (
